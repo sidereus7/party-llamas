c83e642a7c53398e79b4005255f5a04a94e1b666
Sara Jackson <sara@Saras-MacBook-Pro.local>
2017-05-24 11:24:40 -0700

    Add activity to show party status and llamas

--- app/src/main/java/com/sarajackson/apcs/partyllamas/DisplayLlamaActivity.java
+++ app/src/main/java/com/sarajackson/apcs/partyllamas/DisplayLlamaActivity.java
@@ -1,6 +1,5 @@
 package com.sarajackson.apcs.partyllamas;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,12 +7,12 @@ import android.view.View;
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
 
-public class MainActivity extends AppCompatActivity {
+public class DisplayLlamaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_display_llama);
     }
 
     public void llamaAction(View view) {
