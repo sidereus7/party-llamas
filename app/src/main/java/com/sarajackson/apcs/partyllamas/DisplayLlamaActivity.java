a523db8da0040448bc47c31cdf570226d013b60e
Sara Jackson <sara@Saras-MacBook-Pro.local>
2017-05-28 16:27:38 -0700

    Remove unused imports

--- app/src/main/java/com/sarajackson/apcs/partyllamas/DisplayLlamaActivity.java
+++ app/src/main/java/com/sarajackson/apcs/partyllamas/DisplayLlamaActivity.java
@@ -1,7 +1,6 @@
 package com.sarajackson.apcs.partyllamas;
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
